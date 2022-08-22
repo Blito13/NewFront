3070272c4f811ce0af573855f6b307a6491e4b34
Blito13 <lesta4444@hotmail.com>
2022-08-22 12:47:28 -0300

    redux

--- miss/src/App.js
+++ miss/src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
-import './App.css';
-
+import styles from './App.css';
+import Home from "./components/Home"
+import { Provider } from "react-redux";
+import generateStore from "./store";
+import { Route, Routes } from 'react-router-dom';
 function App() {
+  const store = generateStore();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+
+    <Routes>
+      <Route path = '/' element={<Home/>}/>
+      
+      
+    </Routes>
+    
+
+
+
+
+
+
+
+
+  
+
+    </Provider>
   );
 }
 
