5e8e6d6966ea3cf3c34b5e08a11d4d9e63a98261
Blito13 <lesta4444@hotmail.com>
2022-08-23 18:48:02 -0300

    tableMock

--- miss/src/components/Start.jsx
+++ miss/src/components/Start.jsx
@@ -1,14 +1,91 @@
 import styles from "./Start.module.css"
+import DataTable from "react-data-table-component";
+import data from "./data"
 
 
+const Start = (props) => {
+ const flex  = data.map((e) => {
+var newKeys = {
 
-const Start = () => {
+  DecenaDeMil : "",
+  UnidadDeMil : "",
+  Decena : "",
+  Centena : "",
+  Unidad :""
+}
+const finalResult =  Object.assign(e , newKeys)
+
+}
+)
+ console.log(data)
+ 
+ console.log(data)
+    const columns = [
+      {
+        numbers: "Numbers",
+        selector: row => row.name,
+        sortable: true,
+        grow: 2
+      },
+      {
+        name: "Name",
+        selector: row => row.name,
+        sortable: true,
+        grow: 2
+      },
+      {
+        name: "Email",
+        selector: row => row.email,
+        sortable: true,
+        hide: "sm"
+      },
+      {
+        name: "Website",
+        selector: row => row.website ,
+        sortable: true
+      },
+      {
+        name: "Company",
+        selector: row => row.company.name,
+        sortable: true,
+        hide: "md"
+      },
+      {
+        name: "City",
+        selector: row => row.address.city,
+        sortable: true,
+        hide: "md"
+      },
+      {
+        name: "Buttons",
+        button: true,
+        cell: row =>
+          row.showButtons ? (
+            <>
+              <button
+                onClick={() => props.click(row.name)}
+                style={{ marginRight: "5px" }}
+              >
+                Edit
+              </button>
+              <button onClick={() => props.click(row.name)}>Delete</button>
+            </>
+          ) : null
+      }
+    ];
+  
 return (
     <div className={styles.table}>
 
+  <DataTable
+  columns={columns}
+  striped
+  pagination
+  data = {data}
+  >
+  </DataTable>
 
-
-    <table >
+  {/*   <table >
   <tr>
     <th>Name</th>
     <th>Decena de mil</th>
@@ -34,7 +111,7 @@ return (
     <td>4</td>
     <td>4</td>
   </tr>
-</table>
+</table> */}
     </div>
 )
 }
