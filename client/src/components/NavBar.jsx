21c2c73c278be70b0e6f787a82c381bf0688cbc8
blito13 <blito13@github.com>
2023-03-14 23:51:34 -0300

    navBar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,8 +1,9 @@
-import styles from "./NavBar.module.css"
+import styles from "./NavBar.module.css";
 import { Link, useNavigate } from 'react-router-dom';
-import dados   from "../img/fichas.jpg"
-import timer   from "../img/timer.jpg"
-import numbers   from "../img/numbers.jpg"
+import dados   from "../img/fichas.jpg";
+import fly  from "../img/flyMoney.jpg";
+import moneda from "../img/moneda.jpg";
+import numbers   from "../img/numbers.jpg";
 export default function NavBar (){
 const cart     = 5
 var arrayCar = [[],[],[],[]]
@@ -14,63 +15,34 @@ const arrayFav = [[],[],[],[],[]]
 
     return (
         <div className={styles.container}>
-             <div className={styles.leftContent}>
-          <Link to="/" className={styles.divLogo}>
-            <div>
+          <Link to="/">
               <img
                 className={styles.logo}
-                src = {dados}
+                src = "https://thumbs.dreamstime.com/b/velocidad-logo-icon-design-del-dinero-127653477.jpg"
                 alt="logo"
               />
+          </Link>
+             <div className={styles.leftContent}>
+              <span className={styles.title}>
+                <h1>
+                  TuttyCuanty
+                  </h1>
+                </span>
+            <ul className={styles.menu}>
+           
+          <li className={styles.buttonNavBar}>
+          <a href="/">Home</a>
+          </li>
+          <li className={styles.buttonNavBar}>
+          <a href="/start"> List of gambles</a>
+          </li>
+          <li className={styles.buttonNavBar}>
+          <a href="/create"> Lets play</a>
+          </li>
+        
+            </ul>
             </div>
             
-          </Link>
-              <h2 className={styles.leftContent}>TuttiCuanti</h2>
-            </div>
-            <div>
-
-            </div>
-            <ul className={styles.menu}>
-          <Link to="/" className={styles.divLogin}>
-            <button className={styles.loginText/* buttonNavBar */}>Home</button>
-          </Link >
-          <Link to="/Start" className={styles.divLogin}>
-            <button className={styles.loginText/* buttonNavBar */}>List of gambles</button>
-          </Link>
-            <Link to = "/Create" className={styles.divLogin}>
-              <button className={styles.loginText}>Lets PLay</button>
-
-            </Link>
-          <div className={styles.favCarBtns}>
-            <div className={styles.favCarBtns}>
-              <img
-                className={styles.icon}
-                src={timer}
-                alt="Schedules"
-               // onClick={() => validation((valit = "car"))}
-              />
-              {arrayCar.length ? <span className={styles.iconsCartFav}>{arrayCar.length}</span> : null}
-            </div>
-            <div className={styles.favCarBtns}>
-              <img
-                className={styles.icon}
-                src={numbers}
-                alt="Numbers"
-                //onClick={() => validation((valit = "favorites"))}
-              />
-              {arrayFav.length ? <span className={styles.iconsCartFav}>{arrayFav.length}</span> : null}
-            </div>
-          </div>
-            <div className={styles.divLogin} /* onClick={() => loginWithRedirect()} */>
-                <button
-                  className={styles.loginText}
-                  /* onClick={() => loginWithRedirect()} */
-                >
-                  {" "}
-                  Login{" "}
-                </button>
-              </div>
-        </ul>
               
         </div>
     )
