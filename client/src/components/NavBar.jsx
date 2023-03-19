8b68080b8baf4fc819a42cbc4faf19805cc1c29a
blito13 <blito13@github.com>
2023-03-19 15:39:22 -0300

    responsive

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import styles from "./NavBar.module.css";
+import { useState } from "react";
 import { Link, useNavigate } from 'react-router-dom';
 import dados   from "../img/fichas.jpg";
 import fly  from "../img/flyMoney.jpg";
@@ -10,48 +11,49 @@ var arrayCar = [[],[],[],[]]
 
 const fav = [43]
 const arrayFav = [[],[],[],[],[]]
+const [isNavExpanded, setIsNavExpanded] = useState(false)
 
 
 
     return (
-        <div className={styles.container}>
-          <Link to="/">
-              <img
-                className={styles.logo}
-                src = "https://thumbs.dreamstime.com/b/velocidad-logo-icon-design-del-dinero-127653477.jpg"
-                alt="logo"
-              />
-          </Link>
-         
-             <div className={styles.leftContent}>
-              <span className={styles.title}>
-                <h1>
-                  TuttyCuanty
-                  </h1>
-                </span>
-            <ul className={styles.menu}>
-          <li className={styles.buttonNavBar}>
-          <a href="/">Home</a>
+     
+      <nav className={styles.navigation}>
+      <a href="/" className={styles.brandName}>
+        MacroSoft
+      </a>
+      <button className={styles.hamburger} onClick={() => {
+          setIsNavExpanded(!isNavExpanded)
+        }}>
+        {/* icon from heroicons.com */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="white"
+        >
+          <path
+            fillRule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+      <div 
+         className={
+          isNavExpanded ?   styles.navigationMenu2 : styles.navigationMenu
+        }>
+        <ul>
+          <li>
+            <a href="/home">Home</a>
           </li>
-          <li className={styles.buttonNavBar}>
-          <a href="/start"> List of gambles</a>
+          <li>
+            <a href="/about">About</a>
           </li>
-          <li className={styles.buttonNavBar}>
-          <a href="/create"> Lets play</a>
+          <li>
+            <a href="/contact">Contact</a>
           </li>
-            </ul>
-            </div>
-               <div className={styles.rigthContent}>
-                <ul className={styles.rigthMenu}>  
-               <li className={styles.buttonNavBar}>
-                <a>LogIn</a>
-               </li>
-               <li className={styles.buttonNavBar}>
-                <a>About Us</a>
-               </li>
-                </ul>
-               </div>
-               
-        </div>
+        </ul>
+      </div>
+    </nav>
     )
 }
