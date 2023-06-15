9d163ae5b190b8261839f062051cb4d4454213dd
Blito13 <lesta4444@hotmail.com>
2023-06-15 18:37:25 -0300

    infiniteHorizontalTextHome

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -15,17 +15,35 @@ function Home () {
             dispatch(getLogedStatus(token));
         }
     },[dispatch])
+
     return(
-        <div className ={styles.container}>
-        <div className={styles.title}>
-            <h1>
-            renderizar el pozo actual en vivo de las  apuestas con el premio y sus respectivos aciertos en base a la tabla
-            coincidencias de posicion se suman (jugar el mismo numero y la misma posicion) ambas apuestas se suman 
-            la tira se vende toda juntala recompensa debe ser directamente proporcional a el monto apostado (prorrata)
-            varios ganadores 
-            </h1>
-        </div>
-        </div>
+        
+      <div className={styles.container}>
+      <div className={styles.marquee}>
+        <span><b>3,588.39</b></span>
+        <span>XRP<b>0.32</b></span>
+        <span>ETH<b>116.36</b></span>
+        <span>EOS<b>2.44</b></span>
+        <span>USDT<b>1.01</b></span>
+        <span>LTC<b>32.61</b></span>
+        <span>XLM<b>0.10</b></span>
+        <span>TRX<b>0.03</b></span>
+        <span>BSV<b>74.29</b></span>
+        <span>ADA<b>0.04</b></span>
+      </div>
+      <div className={styles.marquee2}>
+        <span>BTC<b>3,588.39</b></span>
+        <span>XRP<b>0.32</b></span>
+        <span>ETH<b>116.36</b></span>
+        <span>EOS<b>2.44</b></span>
+        <span>USDT<b>1.01</b></span>
+        <span>LTC<b>32.61</b></span>
+        <span>XLM<b>0.10</b></span>
+        <span>TRX<b>0.03</b></span>
+        <span>BSV<b>74.29</b></span>
+        <span>ADA<b>0.04</b></span>
+      </div>
+    </div>
     )
 } 
 export default Home;
