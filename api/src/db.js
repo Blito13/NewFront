e2946c5231ae6cbe45ed50cca96f0b01cd6dd29c
Blito13 <lesta4444@hotmail.com>
2023-09-25 15:41:01 -0300

    settingUp

--- api/src/db.js
+++ api/src/db.js
@@ -38,12 +38,6 @@ let sequelize =
     : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/tutti`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  dialectOptions:{
-    ssl:{
-      require : true
-    },
-    ssl:true,
-  }
 });
 /* const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
