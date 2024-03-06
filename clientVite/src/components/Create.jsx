776c6683a947170dd8def9a00acd286ae43f00d7
Blito13 <lesta4444@hotmail.com>
2024-03-06 13:05:04 -0300

    counter play

--- clientVite/src/components/Create.jsx
+++ clientVite/src/components/Create.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 /* import {postGame} from "../redux/actions"; */
 import {validate} from "../Utils"
-
+import {motion}  from 'framer-motion';
 const Create = () => {
     const dispatch = useDispatch();
  
@@ -16,13 +16,14 @@ const Create = () => {
    apuesta : 0,
    numeros :[],
     });
+    const [counter , setCounter] = useState(0);
     useEffect(() => {
         // Verificar si el usuario está autenticado al cargar el componente
         const isLoggedIn = localStorage.getItem('isLoggedIn');
         console.log(isLoggedIn, "motumbo");
        
       }, []);
-    let cards = [,,,,];
+    let cards = [0,1,2,3,4];
 
     const handleChange = (e) => {
         e.preventDefault();
@@ -35,6 +36,7 @@ const Create = () => {
             ...input,
             [e.target.name] : e.target.value
         }));
+        console.log(input)
     }
     const handleSubmit =(e) =>{
     
@@ -44,28 +46,41 @@ const Create = () => {
             apuesta : 0,
             numeros : ""
            })
+           console.log(input)
+    }
+    const variants = {
+        odd : {
+            backgroundColor :'#333',
+            color: '#fff'
+        },
+        even:{
+            backgroundColor : '#fff',
+            color :'#000'
+        }
     }
 return (
     <div className={styles.contenedor}>
      <div className={styles.head}>
-         <form onSubmit={(e) => handleSubmit(e)}>
-        {        
-            [0,1,2,3,4].map(e=>
-                
-                <input className={styles.cards} type='number' min="0" max="9"  placeholder = "apuesta" name='apuesta' /* onChange={(e) =>handleChange(e)}  *//>
-            )
-
-        }
-        </form>
+         {/* <form onSubmit={(e) => handleSubmit(e)}> */}
+         <div style={{display : 'flex', flexDirection : 'column' , gap :'10px'}}>
+            <button className={styles.button} onClick={()=> setCounter(counter => counter+1)}>
+             sum
+             </button>  
+            <motion.div
+            className={styles.box}>
+                {counter}
+            </motion.div>
+            <button className={styles.button} onClick={()=> setCounter(counter => counter-1)}>
+             res
+             </button>  
+         </div>
+      {/*   </form> */}
      </div>
   <div className={styles.form}>
         <form onSubmit={(e) => handleSubmit(e)}>
             <input type='text'  placeholder = "nombre" name='name' onChange={(e) =>handleChange(e)} />
             <input type='number'  placeholder = "apuesta" name='apuesta' onChange={(e) =>handleChange(e)} />
             <input type='text'  placeholder = "tus numeros" name='numeros' onChange={(e) =>handleChange(e)} />
-            <button >
-             Create
-             </button>  
             </form> 
   </div>
             <div className={styles.footer}>
