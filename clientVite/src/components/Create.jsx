04a103dcd2e28140bc370748dee3a4b5623cb65b
blito13 <blito13@github.com>
2024-03-11 13:56:39 -0300

    setting up post play

--- clientVite/src/components/Create.jsx
+++ clientVite/src/components/Create.jsx
@@ -14,8 +14,8 @@ const Create = () => {
     const [numeros, setNumeros] = useState([0, 0, 0, 0, 0]); /* aca cambia */
     const [input, setInput] = useState({
         name : "",
-   apuesta : 0,
-   numeros :[],
+        apuesta : 0,
+        numero :[0,0,0,0,0],
     });
  
     useEffect(() => {
@@ -24,20 +24,33 @@ const Create = () => {
         console.log(isLoggedIn, "motumbo");
        
       }, []);
-  const handleChange = (index, value) => {
-   // Validar que el valor esté entre 0 y 9
-    if (value < 0 || value > 9 || isNaN(value)) {
-      return; // No hacer nada si el valor no está en el rango permitido
-    }
-    const newNumeros = [...numeros];
-    newNumeros[index] = parseInt(value, 10); // Convertir a número entero
-    setNumeros(newNumeros)  
+  const handleChange = (index, e) => {
+    const {name , value} = e.target;
+    console.log(input)
+    const {nombre , apuesta , numero} =  input;
+    name === "sumar"? 
+   (numero[index] +=1) &&
+    setInput({
+      ...input,
+         numero 
+    }):null;
+    name === "restar"?
+    setInput({
+      ...input,
+         numero :numero[index] -1
+
+    }): /* fix this , plus operation still creating a new property */
+    setInput({
+      ...input,
+      [name] : value
+    })   
+    console.log(input)
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     // Aquí puedes hacer lo que quieras con el array de números
-    console.log(numeros);
+    console.log(input.numero);
   };
     const variants = {
         odd : {
@@ -53,26 +66,27 @@ return (
     <div className={styles.contenedor}>
      <div className={styles.head}>
         <form onSubmit={handleSubmit}>
-      {numeros.map((numero, index) => ( /* aca cambia */
+      {input.numero.map((numero, index) => ( /* aca cambia */
         <div key={index}>
-          <button onClick={() => handleChange(index, numero -1)}>-</button>
+          <button name="restar" onClick={(index ,e) => handleChange(index,e)}>-</button>
           <input
             type="number"
             value={numero}
-            onChange={(e) => handleChange(index, e.target.value)}
+            onChange={(index ,e) => handleChange(index, e)}
             min={0}
             max={9}
           />
-          <button onClick={() => handleChange(index, numero +1)}>+</button>
+          <button name = "sumar" onClick={(e) => handleChange(index,e)}>+</button>
         </div>
       ))}
+            <input type='text'  placeholder = "nombre" name='name' onChange={(e) =>handleChange(null ,e)} />
+            <input type='number'  placeholder = "apuesta" name='apuesta' onChange={(e) =>handleChange(null,e)} />
+
       <button type="submit">Guardar</button>
    </form>
      </div>
   <div className={styles.form}>
         <form onSubmit={(e) => handleSubmit(e)}>
-            <input type='text'  placeholder = "nombre" name='name' onChange={(e) =>handleChange(e)} />
-            <input type='number'  placeholder = "apuesta" name='apuesta' onChange={(e) =>handleChange(e)} />
             <input type='text'  placeholder = "tus numeros" name='numeros' onChange={(e) =>handleChange(e)} />
             </form> 
   </div>
