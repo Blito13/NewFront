320b729c49f51df55c1b4d5c4fcd2c42dc01ebde
Blito13 <lesta4444@hotmail.com>
2024-07-22 12:33:14 -0300

    response function

--- api/src/example.js
+++ api/src/example.js
@@ -1,4 +1,21 @@
-const arr1 = [1,2,3,4,5];
-const arr2 = [1,2,1,4,5];
+const arr1 = [8,0,2,4,5];
+const arr2 = [0,1,2,4,5];
+let rev1 = arr1.reverse();
+let rev2 = arr2.reverse();
+let response = []
+ for (let ind = 0 ; ind<5 ; ind ++){
+    if(ind === 0 && rev1[ind] !== rev2[ind]) {
+        
+        response.push(["no hay coincidencias/ no matches at all"])
+        break
+    }
+    if(rev1[ind] !== rev2[ind]){
+        response.push( ["no coinciden"])
+    }else {
+        response.push( ["coinciden"])
+    }
 
-console.log(arr1.toString() ===arr2.toString());
+};
+
+
+console.log(response);
