a3269db2ad8f3527847be298546a897cfbad3c95
Blito13 <lesta4444@hotmail.com>
2024-09-16 17:56:34 -0300

    donde

--- api/src/controllers/authController.js
+++ api/src/controllers/authController.js
@@ -62,7 +62,7 @@ const signIn = async (req , res) => {
     const match =  await Playerxs.findOne({ where: { email : email }});
     if(!match) return res.status(400).json({token:null , message : "user not found"});
     const resc =  await comparePassword( password , match.passWord);
-    if(!resc) return res.status(401).json({token:null , message :"invalid password"});
+    if(!resc) return res.status(401).json({token:null , message :"invalid password or username"});
     const token =  jwt.sign({id:match.id} , SECRET , {expiresIn: 86400})
 
     res.status(200).json({token , message : "ok"})
