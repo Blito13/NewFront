10640033a590c04d0bb46cba50c5013dcc88e731
Blito13 <lesta4444@hotmail.com>
2024-09-23 16:55:26 -0300

    form log

--- clientVite/src/reducers/playReducer.js
+++ clientVite/src/reducers/playReducer.js
@@ -32,9 +32,8 @@ export const playReducer = (state, action) => {
     case PLAY_ACTION_TYPES.LOGIN_SESSION:
       const tk = action.payload;
       let newPlayState = {}
-    console.log(tk, "when everything its ok the data must appear here")
-   /*  console.log(tk.response.data.message , "when the server rejectes the credentials status must be shown here"); */
-    tk.data ? 
+      tk.data 
+      ? 
       newPlayState = {...state, 
       token:tk.data.token,
       status:"on-line",
